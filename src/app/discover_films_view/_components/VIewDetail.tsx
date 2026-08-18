9ed642390773fa6d7196b36b41d5838f447a518f
seongjongju <sjj990480@gmail.com>
2026-08-18 16:05:55 +0900

    불필요한 console 제거

--- src/app/discover_films_view/_components/VIewDetail.tsx
+++ src/app/discover_films_view/_components/VIewDetail.tsx
@@ -54,8 +54,6 @@ const VIewDetail = ({ movieDetail, wishlist, video, viewId }: DetailProps) => {
     const [isSave, setIsSave] = useState<boolean>(wishId);
     const addRecentMovies = useMovieStore((state) => state.addRecentMovies);
 
-    console.log(isSave)
-
     //상세페이지를 들어오면 상세페이지의 정보를 저장한다.   
     useEffect(() => {
         if(!user) return;
